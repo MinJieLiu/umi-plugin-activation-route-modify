617303ef7b69b9bc7a1e993955674b00018af8ae
MinJieLiu <liumingyi_1@163.com>
2020-01-10 15:26:35 +0800

    修复每次切换路由无法 keep 的问题

--- src/wrapChildrenWithKeepAlive.js
+++ src/wrapChildrenWithKeepAlive.js
@@ -1,22 +1,40 @@
 import React from 'react';
 import { KeepAlive } from 'react-activation';
 
-export default (Component, routePath) => props => {
-  const {
-    location: { pathname, search },
-    match: { params },
-  } = props;
+/**
+ * 创建 KeepAliveWrap
+ * - 避免每次渲染后返回新组件
+ */
+function createKeepAliveFunc() {
+  let Component;
+  let routePath;
 
-  return (
-    <KeepAlive
-      name={pathname + search}
-      id={pathname + search}
-      routePath={routePath}
-      pathname={pathname}
-      params={params}
-      search={search}
-    >
-      <Component {...props} />
-    </KeepAlive>
-  );
-};
+  function KeepAliveWrap(props) {
+    const {
+      location: { pathname, search },
+      match: { params },
+    } = props;
+
+    return (
+      <KeepAlive
+        name={pathname + search}
+        id={pathname + search}
+        routePath={routePath}
+        pathname={pathname}
+        params={params}
+        search={search}
+      >
+        <Component {...props} />
+      </KeepAlive>
+    );
+  }
+
+  return (NextComponent, nextRoutePath) => {
+    Component = NextComponent;
+    routePath = nextRoutePath;
+
+    return KeepAliveWrap;
+  };
+}
+
+export default createKeepAliveFunc();
